47d2f4ce7f870006b332071e00942272d37dea5c
Aman <chaudhary.aman01@outlook.com>
2026-06-14 21:33:55 +0530

    auto backup

---
+++ hyprland/custom/keybinds.lua
@@ -0,0 +1,9 @@
+hl.unbind("SUPER + C")
+hl.unbind("SUPER + X")
+hl.bind("SUPER + C", hl.dsp.exec_cmd("kitty -1 nvim"), { description = "App: LazyVim" })
+hl.bind("SUPER + X", hl.dsp.exec_cmd("kitty -1 nvim"), { description = "App: LazyVim" })
+
+hl.bind("ALT + F4", hl.dsp.window.close(), { non_consuming = true })
+
+hl.bind("CTRL+SUPER+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/illogical-impulse/config.json"), { description = "User: Edit shell config" })
+hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), { description = "User: Edit keybinds" })
