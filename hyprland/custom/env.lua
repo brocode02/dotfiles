1e3e85c916735c36a98d70f53714283cbfb98b43
Aman <chaudhary.aman01@outlook.com>
2026-07-08 01:38:17 +0530

    auto backup

--- hyprland/custom/env.lua
+++ hyprland/custom/env.lua
@@ -1,6 +1,4 @@
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("XDG_SESSION_TYPE", "wayland")
-hl.env("GBM_BACKEND", "nvidia-drm")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("NVD_BACKEND", "direct")
 hl.env("WLR_NO_HARDWARE_CURSORS", "1")
