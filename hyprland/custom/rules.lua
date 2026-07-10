ebf101655a179548ac37aa2cd823f6e54ddef418
Aman <chaudhary.aman01@outlook.com>
2026-07-10 23:57:17 +0530

    auto backup

--- hyprland/custom/rules.lua
+++ hyprland/custom/rules.lua
@@ -1,4 +1,3 @@
 hl.window_rule({ match = { class = "^(kitty)$" }, no_blur = true })
 hl.window_rule({ match = { class = "^(kitty)$" }, border_color = "rgba(1793d1DD) rgba(1793d177)" })
 hl.window_rule({ match = { class = "^(Tk)$" }, float = true })
-hl.window_rule({ match = { float = 1 }, center = true })
