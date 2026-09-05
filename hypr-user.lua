c30a73c02d2eab89ef404edfed39977233aa7b4e
Aman <chaudhary.aman01@outlook.com>
2026-09-05 16:00:20 +0530

    auto backup

--- hypr-user.lua
+++ hypr-user.lua
@@ -1,11 +1,11 @@
 ----------------------------------------------------
 ----------------------------------------------------
 hl.bind("SUPER + J", hl.dsp.exec_cmd("caelestia shell drawers toggle bar"), { description = "Shell: Toggle bar" })
--- hl.unbind("SUPER + Z")
--- hl.bind("SUPER + Z", hl.dsp.exec_cmd("/usr/bin/zen-browser"), { description = "open zen broswer" })
 hl.bind("SUPER + Y", hl.dsp.exec_cmd("kitty -1 yazi"), { description = "open yazi file manager" })
 hl.bind("SUPER + I", hl.dsp.exec_cmd("caelestia shell nexus open"), { description = "open setting" })
 -- hl.unbind("SUPER + X")
+-- hl.unbind("SUPER + Z")
+-- hl.bind("SUPER + Z", hl.dsp.exec_cmd("/usr/bin/zen-browser"), { description = "open zen broswer" })
 -- hl.bind("SUPER + X", hl.dsp.exec_cmd("kitty -1 nvchad"), { description = "open lazyvim" })
 -- hl.unbind("SUPER + B")
 -- hl.bind("SUPER+B", hl.dsp.exec_cmd("kitty -1 btop"))
